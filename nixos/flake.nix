16b10f6756ae25d4b770b1f7c9ae73069d537e35
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-10-02 10:29:24 +0200

    chore: create nixos flake

---
+++ nixos/flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    nixos-generators,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        hosts = builtins.readDir ./hosts;
+      in {
+        packages = builtins.listToAttrs (
+          builtins.map
+          (host: {
+            name = host;
+            value = nixos-generators.nixosGenerate {
+              system = system;
+              modules = [
+                {
+                  services.openssh.enable = true;
+                  services.qemuGuest.enable = true;
+                  virtualisation.docker.enable = true;
+                  users.users.nixos = {
+                    isNormalUser = true;
+                    extraGroups = ["wheel"];
+                    openssh.authorizedKeys.keys = [
+                      (builtins.readFile ./id_ed25519.pub)
+                    ];
+                  };
+                  nix.settings.trusted-users = [
+                    "nixos"
+                  ];
+                  system.stateVersion = "25.11";
+                }
+                (./hosts + "/${host}/configuration.nix")
+              ];
+              format = "openstack";
+            };
+          })
+          (builtins.attrNames hosts)
+        );
+      }
+    );
+}
