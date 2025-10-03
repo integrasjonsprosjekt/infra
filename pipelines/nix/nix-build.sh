ccba43582cb913566ebeeb2d2cf0ab959dd8b67c
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-10-03 15:44:40 +0200

    ci(nix): fix path in build script

--- pipelines/nix/nix-build.sh
+++ pipelines/nix/nix-build.sh
@@ -3,7 +3,7 @@
 HOSTS=$(find nixos/hosts -mindepth 1 -maxdepth 1 -type d -printf '%f\n')
 
 for host in $HOSTS; do
-  if [ -f "$host/configuration.nix" ]; then
+  if [ -f "nixos/hosts/$host/configuration.nix" ]; then
     echo "Building and activating configuration for host: $host"
     mkdir -p result
     nix build --extra-experimental-features nix-command --extra-experimental-features flakes "nixos/#$host" --out-link "result/$host"
