f1667a135c1ba075d31a5b810a96e4454a49d30c
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-10-28 17:16:51 +0100

    feat(nix): add cloud init

--- nixos/hosts/backend/configuration.nix
+++ nixos/hosts/backend/configuration.nix
@@ -5,9 +5,12 @@
       frontend = {
         image = "ghcr.io/integrasjonsprosjekt/memora-api:latest";
         ports = [
-          "80:3000"
+          "80:8080"
         ];
+        volumes = [ "/run/secrets/google_application_credentials.json:/google_application_credentials.json" ];
+        environment = { GOOGLE_APPLICATION_CREDENTIALS = "/google_application_credentials.json"; };
       };
     };
   };
+  services.cloud-init.enable = true;
 }
